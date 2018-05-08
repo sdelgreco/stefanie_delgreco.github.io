905658cc2ba802b21698e38bc7bcdc725eeeaa5c
stefanie del greco <stefaniedg714@gmail.com>
2018-05-07 21:23:07 -0400

    ..

---
+++ Homework/citi_bike_api/js/script.js
@@ -0,0 +1,42 @@
+// As a User
+// When I visit the site
+// I expect to see the names of the all the stations
+function stations () {
+  var stations = "All Stations";
+}
+// As a User
+// When I visit the site
+// I expect to see only the first fifty stations on the list
+var topFifty === allStations.topFifty
+
+var stations = [];
+
+stations.forEach
+// As a User
+// When I visit the site
+// I expect to see fifty stations with free bikes only
+var topFifty = allStations.topFifty.splice(0,100);
+
+
+array.forEach (stations(element){
+//element is a single
+});
+// Bonus
+
+// As a User
+// I want to get data from the newer API instead: https://api.citybik.es/v2/networks/citi-bike-nyc
+// So that I can get the most accurate results
+
+// Unreal, Super Epic Bonus
+
+// As a User
+// When I select "number of free bikes" from a dropdown
+// I see fifty stations where the "number of free bikes" is greater than my selection
+
+$.ajax({
+  type: 'GET',
+  url: 'http://api.citybik.es/citi-bike-nyc.json',
+  success: function(stations) {
+    console.log(stations)
+  },
+});
---
+++ Homework/google_maps_example/js/app.js
@@ -0,0 +1,34 @@
+// As a User
+// When I view the site
+// I see a map
+<script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCAaRf4Bk4r-QiUbsnowNSq1QLfD3mibq8&callback=initMap">
+    </script>
+
+// As a User
+// When I view the site
+// I expect to only see NYC
+
+// As a User
+// When I view the site
+// I want to see a marker on NYC
+
+// Bonus:
+
+// As a User
+// When I view the map
+// I want to see the terrain view
+
+// More Bonus:
+
+// As a User
+// When I press a button
+// I see 3 markers appear on the map
+
+// Unbelievable, Seriously???? Bonus
+
+// As a User
+// When I view the site
+// All the CitiBike NYC stations appear on the map as markers
+
+// Hint: Make sure to use this URL: https://api.citybik.es/v2/networks/citi-bike-nyc
